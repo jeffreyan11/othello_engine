210ef4970c3ff963ad49782b7208892413f17570
Jeffrey <jeffreyan07@gmail.com>
2014-03-12 14:02:52 -0700

    Fixed an error in Board with variable declarations.

--- board.cpp
+++ board.cpp
@@ -1,6 +1,33 @@
 #include "board.h"
 #include <iostream>
 
+const bitbrd MOVEMASK[64] = {
+0x0000000000000001, 0x0000000000000002, 0x0000000000000004, 0x0000000000000008,
+0x0000000000000010, 0x0000000000000020, 0x0000000000000040, 0x0000000000000080,
+0x0000000000000100, 0x0000000000000200, 0x0000000000000400, 0x0000000000000800,
+0x0000000000001000, 0x0000000000002000, 0x0000000000004000, 0x0000000000008000,
+0x0000000000010000, 0x0000000000020000, 0x0000000000040000, 0x0000000000080000,
+0x0000000000100000, 0x0000000000200000, 0x0000000000400000, 0x0000000000800000,
+0x0000000001000000, 0x0000000002000000, 0x0000000004000000, 0x0000000008000000,
+0x0000000010000000, 0x0000000020000000, 0x0000000040000000, 0x0000000080000000,
+0x0000000100000000, 0x0000000200000000, 0x0000000400000000, 0x0000000800000000,
+0x0000001000000000, 0x0000002000000000, 0x0000004000000000, 0x0000008000000000,
+0x0000010000000000, 0x0000020000000000, 0x0000040000000000, 0x0000080000000000,
+0x0000100000000000, 0x0000200000000000, 0x0000400000000000, 0x0000800000000000,
+0x0001000000000000, 0x0002000000000000, 0x0004000000000000, 0x0008000000000000,
+0x0010000000000000, 0x0020000000000000, 0x0040000000000000, 0x0080000000000000,
+0x0100000000000000, 0x0200000000000000, 0x0400000000000000, 0x0800000000000000,
+0x1000000000000000, 0x2000000000000000, 0x4000000000000000, 0x8000000000000000,
+};
+const bitbrd NORTHLINE = 0x00000000000000FF;
+const bitbrd SOUTHLINE = 0xFF00000000000000;
+const bitbrd EASTLINE = 0x8080808080808080;
+const bitbrd WESTLINE = 0x0101010101010101;
+const bitbrd NELINE = 0x80808080808080FF;
+const bitbrd NWLINE = 0x01010101010101FF;
+const bitbrd SWLINE = 0xFF01010101010101;
+const bitbrd SELINE = 0xFF80808080808080;
+
 /*
  * Make a standard 8x8 othello board and initialize it to the standard setup.
  */
@@ -64,8 +91,7 @@ bool Board::isDone() {
 bool Board::hasMoves(Side side) {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            Move move(i, j);
-            if (checkMove(&move, side)) return true;
+            if (checkMove(i, j, side)) return true;
         }
     }
     return false;
@@ -81,6 +107,35 @@ bool Board::checkMove(Move *m, Side side) {
     int X = m->getX();
     int Y = m->getY();
 
+    // Make sure the square hasn't already been taken.
+    if (occupied(X, Y)) return false;
+
+    Side other = (side == BLACK) ? WHITE : BLACK;
+    for (int dx = -1; dx <= 1; dx++) {
+        for (int dy = -1; dy <= 1; dy++) {
+            if (dy == 0 && dx == 0) continue;
+
+            // Is there a capture in that direction?
+            int x = X + dx;
+            int y = Y + dy;
+            if (onBoard(x, y) && get(other, x, y)) {
+                do {
+                    x += dx;
+                    y += dy;
+                } while (onBoard(x, y) && get(other, x, y));
+
+                if (onBoard(x, y) && get(side, x, y)) return true;
+            }
+        }
+    }
+    return false;
+}
+
+/*
+ * Overloaded function taking x, y instead of a move object for internal use.
+ * Pass is not an option here.
+*/
+bool Board::checkMove(int X, int Y, Side side) {
     // Make sure the square hasn't already been taken.
     if (occupied(X, Y)) return false;
 
@@ -189,10 +244,10 @@ vector<Move *> Board::getLegalMoves(Side side) {
     vector<Move *> result;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            Move * myMove = new Move(i, j);
-            if (checkMove(myMove, side))
+            if (checkMove(i, j, side)) {
+                Move * myMove = new Move(i, j);
                 result.push_back(myMove);
-            else delete myMove;
+            }
         }
     }
     return result;
